abcd601a1b644ba5ec9421fb64a32664a98f6122
Tommyluo <tluo12321@yahoo.com>
2015-03-07 19:13:59 -0800

    v1.3 minor bug fixes

--- client/main.js
+++ client/main.js
@@ -1,125 +1,145 @@
 'use strict';
 
 $(document).ready(function() {
-  $('#start').click(startGame);
+
+  $('#start').click(function() {
+    var countTurn = 0;
+    var $p1 = $('#p1box').val();
+    var $p2 = $('#p2box').val();
+    var pArr = [];
+    pArr.push($p1, $p2);
+    var turn = pArr[Math.floor(Math.random() * pArr.length)];
+
+    if (!$p1 || !$p2) {
+      alert('Please enter a tag');
+      return;
+    }
+
+    alert('It is player ' + turn + 's turn');
+
+    $('.box').click(function() {
+      // debugger;
+      $(this).html($('<h3>' + turn + '</h3>'));
+
+      if (turn === $p1) {
+        turn = $p2;
+        checkWin($p1);
+        // $(this).prop('disabled', true);
+        console.log('P1: ' + $p1);
+        console.log('It is: ' + turn + ' turn');
+      } else {
+        turn = $p1;
+        checkWin($p2);
+        // $(this).prop('disabled', true);
+        console.log('P1: ' + $p2);
+        console.log('It is: ' + turn + ' turn');
+      }
+      countTurn++;
+
+      function checkWin(player) {
+        //Check top row
+        if ($('#board').find('#1').html() !== '') {
+          if ($('#board').find('#1').html() === $('#board').find('#2').html()) {
+            if ($('#board').find('#1').html() === $('#3').html()) {
+              alert(player + ' Won!');
+              console.log('top row win');
+              gameReset();
+            }
+          }
+        }
+        //Check mid row
+        if ($('#board').find('#4').html() !== '') {
+          if ($('#board').find('#4').html() === $('#board').find('#5').html()) {
+            if ($('#board').find('#4').html() === $('#6').html()) {
+              alert(player + ' Won!');
+              console.log('mid row win');
+              gameReset();
+            }
+          }
+        }
+        //Check bot row
+        if ($('#board').find('#7').html() !== '') {
+          if ($('#board').find('#7').html() === $('#board').find('#8').html()) {
+            if ($('#board').find('#7').html() === $('#9').html()) {
+              alert(player + ' Won!');
+              console.log('bot row win');
+              gameReset();
+            }
+          }
+        }
+        //Check left column
+        if ($('#board').find('#1').html() !== '') {
+          if ($('#board').find('#1').html() === $('#board').find('#4').html()) {
+            if ($('#board').find('#1').html() === $('#7').html()) {
+              alert(player + ' Won!');
+              console.log('left col win');
+              gameReset();
+            }
+          }
+        }
+        //Check mid column
+        if ($('#board').find('#2').html() !== '') {
+          if ($('#board').find('#2').html() === $('#board').find('#5').html()) {
+            if ($('#board').find('#2').html() === $('#8').html()) {
+              alert(player + ' Won!');
+              console.log('mid col win');
+              gameReset();
+            }
+          }
+        }
+        //Check right column
+        if ($('#board').find('#3').html() !== '') {
+          if ($('#board').find('#3').html() === $('#board').find('#6').html()) {
+            if ($('#board').find('#3').html() === $('#9').html()) {
+              alert(player + ' Won!');
+              console.log('right col win');
+              gameReset();
+            }
+          }
+        }
+        //Check left diagonal
+        if ($('#board').find('#1').html() !== '') {
+          if ($('#board').find('#1').html() === $('#board').find('#5').html()) {
+            if ($('#board').find('#1').html() === $('#9').html()) {
+              alert(player + ' Won!');
+              console.log('left diag win');
+              gameReset();
+            }
+          }
+        }
+        //Check right diagonal
+        if ($('#board').find('#3').html() !== '') {
+          if ($('#board').find('#3').html() === $('#board').find('#5').html()) {
+            if ($('#board').find('#3').html() === $('#7').html()) {
+              alert(player + ' Won!');
+              console.log('right diag win');
+              gameReset();
+            }
+          }
+        }
+
+        if (countTurn === 9) {
+          alert('Tie Game!');
+          gameReset();
+        }
+      }
+    });
+
+    $('#newgame').click(function() {
+      $('td').text('');
+      countTurn = 0;
+      $('#board').fadeIn(function() {
+        $('#board').animate({opacity: 1}, 500);
+        $('#start, #gameMsg').show();
+      });
+    });
+
+    function gameReset() {
+      $('#board').fadeOut(function() {
+        $('#board').animate({opacity: 0}, 500);
+        $('#start, #gameMsg').hide();
+      });
+    }
+  });
+
 });
-
-function startGame() {
-  var countTurn = 0;
-  var p1Input = $('#p1box').val();
-  var p2Input = $('#p2box').val();
-  var pArr = [];
-  pArr.push(p1Input, p2Input);
-
-  //randomize between p1 and p2
-  var turn = pArr[Math.floor(Math.random() * pArr.length)];
-  if (turn === p1Input) {
-    turn = p2Input;
-  } else {
-    turn = p1Input;
-    console.log('It is: ' + turn + ' turn');
-  }
-
-  alert('It is player ' + turn + 's turn');
-
-  $('#newgame').click(function() {
-    $('td').text('');
-    countTurn = 0;
-  });
-
-  //when box is clicked
-  $('#board td').click(function() {
-    if (countTurn % 2 === 0) {
-  //paint p1 text
-      $(this).text(p1Input);
-      countTurn++;
-      checkWin(p1Input);
-      console.log('P1: ' + p1Input);
-    } else {
-  //paint p2 text
-      $(this).text(p2Input);
-      countTurn++;
-      checkWin(p2Input);
-      console.log('P2: ' + p2Input);
-    }
-    console.log('Turn: ' + countTurn);
-
-    function checkWin(player) {
-      //Check top row
-      if ($('#board').find('#1').text() !== '') {
-        if ($('#board').find('#1').text() === $('#board').find('#2').text()) {
-          if ($('#board').find('#1').text() === $('#3').text()) {
-            alert(player + ' Won!');
-          }
-        }
-      }
-
-      //Check mid row
-      if ($('#board').find('#4').text() !== '') {
-        if ($('#board').find('#4').text() === $('#board').find('#5').text()) {
-          if ($('#board').find('#4').text() === $('#6').text()) {
-            alert(player + ' Won!');
-          }
-        }
-      }
-
-      //Check bot row
-      if ($('#board').find('#7').text() !== '') {
-        if ($('#board').find('#7').text() === $('#board').find('#8').text()) {
-          if ($('#board').find('#7').text() === $('#9').text()) {
-            alert(player + ' Won!');
-          }
-        }
-      }
-
-      //Check left column
-      if ($('#board').find('#1').text() !== '') {
-        if ($('#board').find('#1').text() === $('#board').find('#4').text()) {
-          if ($('#board').find('#1').text() === $('#7').text()) {
-            alert(player + ' Won!');
-          }
-        }
-      }
-
-      //Check mid column
-      if ($('#board').find('#2').text() !== '') {
-        if ($('#board').find('#2').text() === $('#board').find('#5').text()) {
-          if ($('#board').find('#2').text() === $('#8').text()) {
-            alert(player + ' Won!');
-          }
-        }
-      }
-
-      //Check right column
-      if ($('#board').find('#3').text() !== '') {
-        if ($('#board').find('#3').text() === $('#board').find('#6').text()) {
-          if ($('#board').find('#3').text() === $('#9').text()) {
-            alert(player + ' Won!');
-          }
-        }
-      }
-
-      //Check left diagonal
-      if ($('#board').find('#1').text() !== '') {
-        if ($('#board').find('#1').text() === $('#board').find('#5').text()) {
-          if ($('#board').find('#1').text() === $('#9').text()) {
-            alert(player + ' Won!');
-          }
-        }
-      }
-
-      //Check right diagonal
-      if ($('#board').find('#3').text() !== '') {
-        if ($('#board').find('#3').text() === $('#board').find('#5').text()) {
-          if ($('#board').find('#3').text() === $('#7').text()) {
-            alert(player + ' Won!');
-          }
-        }
-      }
-
-      if (countTurn === 9) {
-        alert('Tie Game!');
-      }
-    }
-  });
-}
